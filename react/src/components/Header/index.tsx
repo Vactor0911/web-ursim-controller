e75a509ab3d75bf9cfe3396a76673027aec6dd3d
Vactor0911 <vactor0911@gmail.com>
2026-01-31 13:50:10 +0000

    헤더 수정

--- react/src/components/Header/index.tsx
+++ react/src/components/Header/index.tsx
@@ -1,6 +1,5 @@
 import { AppBar, Stack } from "@mui/material";
 import IconButton from "./IconButton";
-import GamesRoundedIcon from "@mui/icons-material/GamesRounded";
 import ControlCameraRoundedIcon from "@mui/icons-material/ControlCameraRounded";
 import { Menu } from "../../states";
 
@@ -15,15 +14,9 @@ const Header = () => {
       <Stack direction="row" alignItems="center">
         {/* 메뉴 버튼 */}
         <IconButton
-          icon={<GamesRoundedIcon fontSize="large" />}
-          label="버튼"
-          menu={Menu.Button}
-        />
-
-        <IconButton
           icon={<ControlCameraRoundedIcon fontSize="large" />}
-          label="조이스틱"
-          menu={Menu.Joystick}
+          label="Move"
+          menu={Menu.Button}
         />
       </Stack>
     </AppBar>
